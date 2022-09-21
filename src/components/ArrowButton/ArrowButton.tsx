53d0b429654bf52ebc266a023c081e0e2f06c33f
Mario <mariob360@gmail.com>
2022-09-21 14:49:20 -0700

    add svg to arrow button component

--- src/components/ArrowButton/ArrowButton.tsx
+++ src/components/ArrowButton/ArrowButton.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyledArrowButton } from './ArrowButtonStyles';
-
+import RightArrow from '../../assets/icon-arrow-right.svg'
+import LeftArrow from '../../assets/icon-arrow-left.svg'
 
 const ArrowButton = () => {
   return (
-    <StyledArrowButton>StyledArrowButton</StyledArrowButton >
+    <StyledArrowButton>
+      <img src={RightArrow} alt="Right Arrow" />
+    </StyledArrowButton>
   )
 }
 
--- src/components/ArrowButton/ArrowButtonStyles.tsx
+++ src/components/ArrowButton/ArrowButtonStyles.tsx
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
+// need to be 64x64px
 
 export const StyledArrowButton = styled.button`
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
   background: var(--black);
   color: #FFFFFF;
+
   :hover {
     background: var(--galactic-blue);
   }
-`
+
+// if its left pointing then 8px padding on the right
+// if its right pointing then 8px padding on the left
+//  $//{({ direction }) => direction === 'left' ? 'padding-right: 8px;' : 'padding-left: 8px;'}
+
+`;
