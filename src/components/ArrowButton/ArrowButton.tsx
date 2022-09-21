0e7d5e911a9ec9e45dab9f4fe65350e5d75203e3
Mario <mariob360@gmail.com>
2022-09-21 13:02:49 -0700

    add base component for arrow button

---
+++ src/components/ArrowButton/ArrowButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledArrowButton } from './ArrowButtonStyles';
+
+type Props = {}
+
+const ArrowButton = (props: Props) => {
+  return (
+    <StyledArrowButton>StyledArrowButton</StyledArrowButton>
+  )
+}
+
+export default ArrowButton
