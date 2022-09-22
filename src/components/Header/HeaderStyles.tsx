6219a0f3fdde41a91407d5aee8b3357925756022
Mario <mariob360@gmail.com>
2022-09-22 16:15:21 -0700

    add bottom margin to header component

--- src/components/Header/HeaderStyles.tsx
+++ src/components/Header/HeaderStyles.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
-
 export const HeaderContainer = styled.header`
 
   max-width: var(--max-width);
-  width: var(--max-width);
+  width: 100%;
+  margin-bottom: 4.125rem;
 
   div {
     display: flex;
