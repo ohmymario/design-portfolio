95a0d3eabba5e9f0455b2036966d56776aff6811
Mario <mariob360@gmail.com>
2022-09-23 21:38:57 -0700

    add size prop to Tile component

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -3,12 +3,13 @@ import { TileContainer } from "./TileStyles";
 
 type Props = {
   title: string,
+  size: string,
   children: React.ReactNode,
 };
 
 const Tile = (props: Props) => {
   return (
-    <TileContainer>
+    <TileContainer size={props.size}>
       {props.children}
       <h4>{props.title}</h4>
     </TileContainer>
