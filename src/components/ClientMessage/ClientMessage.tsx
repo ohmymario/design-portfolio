46fffa695cbe69a05d24625121dadce7f4894477
Mario <mariob360@gmail.com>
2022-09-28 16:24:58 -0700

    add base clientmessage component

---
+++ src/components/ClientMessage/ClientMessage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ConButton from '../ConButton/ConButton';
+
+type Props = {}
+
+const ClientMessage = (props: Props) => {
+  return (
+    <div>
+      <h1></h1>
+      <p></p>
+      <ConButton primary={false} >Free Consultation</ConButton>
+    </div>
+  )
+}
+
+export default ClientMessage
---
+++ src/components/ClientMessage/ClientMessageStyles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+export const StyledClientMessage = styled.div`
+//540 x 306
+
+  width: 540px;
+  height: 306px;
+
+  h1 {
+
+  }
+
+  p {
+
+  }
+
+
+`;
