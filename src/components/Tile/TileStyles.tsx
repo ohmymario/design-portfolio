95b1ce64bf4baeb3fa6215056564c3f2f8b5f0f4
Mario <mariob360@gmail.com>
2022-09-28 14:16:16 -0700

    add styles for Tile component

--- src/components/Tile/TileStyles.tsx
+++ src/components/Tile/TileStyles.tsx
@@ -1,35 +1,55 @@
 import styled from 'styled-components';
 
-export const TileContainer = styled.div`
+interface Props {
+  size: string,
+};
+
+export const TileContainer = styled.div<Props>`
 
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-
-  height: 364px;
-  width: 354px;
-
   padding: 24px;
   border-radius: 8px;
   font-size: 24px;
-
   background: var(--galactic-blue);
 
 
-  // full
-  /* 128x192 */
+  // width and height 100$ of parent
+  width: 100%;
+  height: 100%;
 
-  // quarter
-  /* 64x64 */
+  ${({ size }) => size === 'full' &&
+    `
+    // width: 354px;
+    // height: 364px;
+    grid-column: span 2;
+    grid-row: span 2;
+    `}
 
-  // half
-  /* 128x64 */
+  ${({ size }) => size === 'half' &&
+    `
+    // width: 354px;
+    // height: 182px;
+    grid-column: span 2;
+    grid-row: span 1;
+    `}
+
+  ${({ size }) => size === 'quarter' &&
+    `
+    // width: 164px;
+    // height: 182px;
+    grid-column: span 1;
+    grid-row: span 1;
+    `}
+
 
   img {
-
+    align-self: flex-end;
   }
 
   h4 {
+
   }
 
 `;
