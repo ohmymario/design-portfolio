9f6a0b5525e23cebf21c3427d8e71a1966a82564
Mario <mariob360@gmail.com>
2022-10-11 13:09:04 -0700

    Add union type to arrow button component

--- src/components/ArrowButton/ArrowButton.tsx
+++ src/components/ArrowButton/ArrowButton.tsx
@@ -4,14 +4,15 @@ import RightArrow from '../../assets/icon-arrow-right.svg'
 import LeftArrow from '../../assets/icon-arrow-left.svg'
 
 type ArrowProps = {
-  direction: string;
+  direction: "left" | "right";
 }
 
 const ArrowButton = (props: ArrowProps) => {
   const { direction } = props;
   return (
     <StyledArrowButton direction={direction}>
-      {direction === 'right' ? <img src={RightArrow} alt="Move Right" /> : <img src={LeftArrow} alt="Move Left" />}
+      {direction === 'right' && <img src={RightArrow} alt="Move Right" />}
+      {direction === 'left' && <img src={LeftArrow} alt="Move Left" />}
     </StyledArrowButton>
   )
 };
