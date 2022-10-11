9220432b7ded6068517687992467b2c4c9bcd748
Mario <mariob360@gmail.com>
2022-10-11 16:19:43 -0700

    add base for footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
