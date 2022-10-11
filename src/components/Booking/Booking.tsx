056a28b60b12070c4c027c97c1e8467b3a465ff9
Mario <mariob360@gmail.com>
2022-10-11 15:36:49 -0700

    add booking component

---
+++ src/components/Booking/Booking.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BookingContainer, BookingText, BookingButton } from './BookingStyles'
+import ConButton from '../ConButton/ConButton'
+
+type Props = {}
+
+const data = {
+  title: `Book a call with me`,
+  text: `I’d love to have a chat to see how I can help you. The best first step is for us to discuss your project during a free consultation. Then we can move forward from there.`,
+  primary: false,
+}
+
+const Booking = (props: Props) => {
+
+  const { title, text, primary } = data
+
+  return (
+    <BookingContainer>
+      <BookingText>
+        <h2>{title}</h2>
+        <p>{text}</p>
+      </BookingText>
+
+      <BookingButton>
+        <ConButton primary={primary}>Free Consultation</ConButton>
+      </BookingButton>
+    </BookingContainer>
+
+  )
+}
+
+export default Booking
---
+++ src/components/Booking/BookingStyles.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const BookingContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: var(--max-width);
+  width: 100%;
+
+  padding: 5rem 4rem;
+  background-color: var(--black);
+  border-radius: 10px;
+  align-items: center;
+
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+
+`;
+
+export const BookingText = styled.div`
+
+  width: 540px;
+
+  h2 {
+    color: var(--light-cream);
+    margin-bottom: 25px;
+  }
+
+  p {
+    color: var(--light-cream);
+  }
+
+`;
+
+export const BookingButton = styled.div`
+  padding: 0 30px;
+`;
