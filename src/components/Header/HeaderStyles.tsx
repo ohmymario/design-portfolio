537e933df5b92cd782fa57fa0080224762bce31c
Mario <mariob360@gmail.com>
2022-10-11 18:24:18 -0700

    remove unnecessary css in headerstyles

--- src/components/Header/HeaderStyles.tsx
+++ src/components/Header/HeaderStyles.tsx
@@ -1,15 +1,10 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
-
   max-width: var(--max-width);
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   margin-bottom: 4.125rem;
-
-  div {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-  }
 `;
