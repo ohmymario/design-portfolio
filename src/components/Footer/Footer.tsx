2b3c1893fc7e5cfc2fc8b470da0f017eb5faedf8
Mario <mariob360@gmail.com>
2022-10-12 18:55:42 -0700

    create footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Header from '../Header/Header';
 
 type Props = {}
 
 const Footer = (props: Props) => {
-  return (
-    <div>
-      <div></div>
-      <div></div>
-    </div>
-  )
+  return <Header />
 }
 
 export default Footer
