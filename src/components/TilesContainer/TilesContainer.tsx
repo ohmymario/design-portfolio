8e470ac7312d91da34a010f9afe9aa51a19f013d
Mario <mariob360@gmail.com>
2022-11-01 13:19:59 -0700

    pass colors into tiles container

--- src/components/TilesContainer/TilesContainer.tsx
+++ src/components/TilesContainer/TilesContainer.tsx
@@ -16,22 +16,22 @@ type Props = {
 const TilesContainer = (props: Props) => {
   return (
     <TilesWrapper>
-      <Tile title="Graphic Design" size="full" >
+      <Tile title="Graphic Design" size="full" color="galactic-blue">
         <img src={graphicDesign} alt="" />
       </Tile>
-      <Tile title="UI/UX" size="quarter">
+      <Tile title="UI/UX" size="quarter" color="summer-yellow">
         <img src={uiux} alt="" />
       </Tile>
-      <Tile title="Apps" size="quarter">
+      <Tile title="Apps" size="quarter" color="pink">
         <img src={apps} alt="" />
       </Tile>
-      <Tile title="Photography" size="half">
+      <Tile title="Photography" size="half" color="cyan">
         <img src={photography} alt="" />
       </Tile>
-      <Tile title="Illustrations" size="half">
+      <Tile title="Illustrations" size="half" color="light-red">
         <img src={illustrations} alt="" />
       </Tile>
-      <Tile title="Motion Graphics" size="half">
+      <Tile title="Motion Graphics" size="half" color="dark-purple">
         <img src={motiongraphics} alt="" />
       </Tile>
 
