84ebfd0985159750d8989c338e2dc0f580871235
Mario <mariob360@gmail.com>
2022-11-01 13:05:11 -0700

    add union type for tile colors

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -4,14 +4,27 @@ import { TileContainer } from "./TileStyles";
 type Props = {
   title: string,
   size: string,
+  color: Color,
   children: React.ReactNode,
 };
 
+type Color =
+  'galactic-blue' |
+  'summer-yellow' |
+  'pink' |
+  'light-red' |
+  'cyan' |
+  'dark-purple'
+  ;
+
 const Tile = (props: Props) => {
+
+  const { title, size, color, children } = props;
+
   return (
-    <TileContainer size={props.size}>
-      {props.children}
-      <h4>{props.title}</h4>
+    <TileContainer size={size} color={color}>
+      {children}
+      <h4>{title}</h4>
     </TileContainer>
   );
 };
