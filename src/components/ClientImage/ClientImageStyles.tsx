1d12ac9190a140f82ff1a6a6720870176d2be325
Mario <mariob360@gmail.com>
2022-11-08 19:13:00 -0800

    add media query to client image for mobile

--- src/components/ClientImage/ClientImageStyles.tsx
+++ src/components/ClientImage/ClientImageStyles.tsx
@@ -4,6 +4,12 @@ export const StyledClientImage = styled.img`
   width: 445px;
   height: 445px;
   border-radius: 50%;
+
+  @media screen and (max-width: 1050px) {
+    width: 364px;
+    height: 364px;
+  }
+
 `
 
 export default StyledClientImage;
