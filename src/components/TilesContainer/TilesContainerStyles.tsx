b1e1e5645523d19aa860dcb5723350460939edd1
Mario <mariob360@gmail.com>
2022-11-08 19:05:56 -0800

    add tablet media query for tiles container

--- src/components/TilesContainer/TilesContainerStyles.tsx
+++ src/components/TilesContainer/TilesContainerStyles.tsx
@@ -17,4 +17,12 @@ export const TilesWrapper = styled.div`
 
   margin-bottom: 8.5rem;
 
+  @media screen and (max-width: 1050px) {
+    padding: 0 40px;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 1fr 1fr 1fr;
+    height: 570px;
+    margin-bottom: 7.5rem;
+  }
+
 `;
