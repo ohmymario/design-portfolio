38ec31090fd01161ae20755594ae97c230b9917e
Mario <mariob360@gmail.com>
2022-11-15 20:17:20 -0800

    add weight 500 for Plus Jakarta Sans

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -2,7 +2,7 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700&display=swap');
   :root {
     --black: #030303;
     --medium-brown: #7A746E;
