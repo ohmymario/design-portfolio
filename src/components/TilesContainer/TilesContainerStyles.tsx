f29445fc516eed617e013f25632dece38792221b
Mario <mariob360@gmail.com>
2022-11-15 19:52:25 -0800

    add mobile media query for tiles container

--- src/components/TilesContainer/TilesContainerStyles.tsx
+++ src/components/TilesContainer/TilesContainerStyles.tsx
@@ -18,11 +18,19 @@ export const TilesWrapper = styled.div`
   margin-bottom: 8.5rem;
 
   @media screen and (max-width: 1050px) {
-    padding: 0 40px;
+    padding: 0 2.5rem;
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: 1fr 1fr 1fr;
     height: 570px;
     margin-bottom: 7.5rem;
+  }
+
+  @media screen and (max-width: 650px) {
+    padding: 0 1rem;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(6, 1fr);
+    height: 1164px;
+    margin-bottom: 6.25rem;
   }
 
 `;
