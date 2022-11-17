de3be9f0b7d34302189e242bc6f077f950d89e51
Mario <mariob360@gmail.com>
2022-11-16 18:19:58 -0800

    remove old comments from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import GlobalStyle from './theme/globalStyles';
-import ArrowButton from './components/ArrowButton/ArrowButton';
-import ConButton from './components/ConButton/ConButton';
 import Layout from './pages/Layout';
 
 function App() {
@@ -9,14 +7,6 @@ function App() {
     <>
       <GlobalStyle />
       <Layout />
-      {/* <h1>Heading Large</h1>
-      <h2>Heading Medium</h2>
-      <h3>Heading Small</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <ArrowButton direction={"left"} />
-      <ArrowButton direction={"right"} />
-      <ConButton primary >Free Consultation</ConButton>
-      <ConButton primary={false} >Free Consultation</ConButton> */}
     </>
   );
 }
